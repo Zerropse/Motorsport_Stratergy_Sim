250ebf4807bee0319501a509760e576e3bbf6c1c
Kush Singh <zerrsingh@gmail.com>
2026-02-16 22:18:13 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from tire import Tire
+from car import Car
 
 tire = Tire("soft")
+car = Car(tire, fuel=100)
 
 for lap in range(10):
-    print(f"Lap {lap+1}: Grip = {tire.get_grip():.3f}")
-    tire.update()
+    lap_time = car.compute_lap_time()
+    
+    print(f"Lap {lap+1}: Time = {lap_time:.2f}s | Grip = {tire.get_grip():.3f} | Fuel = {car.fuel}")
+
+    car.update()
